9f5a623017ab75f3105c8d4b4f90b0b45a6c7203
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-11-04 11:16:49 +0200

    added responsive to searchCategory

--- src/components/Category/VerticalCategoryList/VerticalCategoryList.tsx
+++ src/components/Category/VerticalCategoryList/VerticalCategoryList.tsx
@@ -4,11 +4,13 @@ import styles from "./VerticalCategoryList.module.scss"
 
 export function VerticalCategoryList() {
   return (
-    <div>
-        <h2 className={styles.title}>Categories</h2>
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>Categories</h2>
+      <div className={styles.container}>
         {categories.map((category) => (
             <CategoryCard isVertical key={category.name} category={category} />
       ))}
+      </div>
     </div>
   )
 }
