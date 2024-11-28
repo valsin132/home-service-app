b0603d98ad4835855b56aeeee9446d606129ca91
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-11-28 13:18:35 +0200

    added to book appointment only for loggined users

--- frontend/src/pages/BusinessPage/BusinessPage.tsx
+++ frontend/src/pages/BusinessPage/BusinessPage.tsx
@@ -7,17 +7,30 @@ import { BusinessGallery } from "@/components/Business/BusinessGallery/BusinessG
 import { BusinessSimilarBusinesses } from "@/components/Business/BusinessSimilarBusinesses/BusinessSimilarBusinesses";
 import { Button } from "@/components/Button/Button";
 import { PiNotePencil } from "react-icons/pi";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { BookingDateTime } from "@/components/booking/BookingDateTime";
+import { Toast } from "@/components/Toast/Toast";
+import { UserContext } from "@/contexts/UserContext";
 import styles from "./BusinessPage.module.scss";
 
 export function BusinessPage() {
+  const { isLoggedIn } = useContext(UserContext);
   const { id } = useParams();
   const { data: business } = useBusiness(id) as { data: Business };
+  const [toastVisible, setToastVisible] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const openSidebar = () => setIsOpen(true);
   const closeSidebar = () => setIsOpen(false);
+
+  const handleBookingClick = () => {
+    if (!isLoggedIn) {
+      setToastVisible(true);
+      setTimeout(() => setToastVisible(false), 3000);
+    } else {
+      openSidebar();
+    }
+  };
 
   return (
     <>
@@ -29,7 +42,7 @@ export function BusinessPage() {
             <BusinessGallery business={business} />
           </div>
           <div className={styles.businessBodyRightCol}>
-            <Button onClick={openSidebar} className={styles.bookingBtn}>
+            <Button onClick={handleBookingClick} className={styles.bookingBtn}>
               <PiNotePencil className={styles.bookingIcon} />
               <span>Book Appointment</span>
             </Button>
@@ -40,6 +53,12 @@ export function BusinessPage() {
       <Sidebar isOpen={isOpen} onClose={closeSidebar}>
         <BookingDateTime />
       </Sidebar>
+      <Toast
+        isVisible={toastVisible}
+        content="Please log in to book an appointment."
+        toastType="Warning"
+        onClick={() => setToastVisible(false)}
+      />
     </>
   );
 }
